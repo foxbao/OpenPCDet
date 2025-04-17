ae62f65ea9f99fca1785eccfdc4a8c89f6b2846c
佳立 鲍 <jiali.bao@kunlang.cn>
2025-04-17 13:44:31 +0800

    1. completed multi_classes_agnostic_nms in model_nms_utils.py, which filters the prediction with multiple class threshold 2. add if post_process_cfg.MULTI_CLASSES_SCORE_THRESH: in detector3d_template.py to activate multi_classes_agnostic_nms when MULTI_CLASSES_SCORE_THRESH is given in yaml

--- pcdet/models/model_utils/model_nms_utils.py
+++ pcdet/models/model_utils/model_nms_utils.py
@@ -3,11 +3,37 @@ import torch
 from ...ops.iou3d_nms import iou3d_nms_utils
 
 
-def multi_classes_agnostic_nms(box_scores, box_preds, nms_config, score_thresh=None):
+def multi_classes_agnostic_nms(box_scores, box_preds, label_preds,nms_config, score_thresh=None,multi_classes_score_thresh=None):
     src_box_scores = box_scores
-    
-    raise NotImplementedError
+    if multi_classes_score_thresh is not None:
+        # 构建每个box的分数阈值，按label_preds映射
+        numeric_thresh_list = [float(item.split(":")[1]) for item in multi_classes_score_thresh]
 
+        numeric_thresh_list = [float("inf")] + numeric_thresh_list
+        thresh_tensor_lookup = torch.tensor(numeric_thresh_list, device=box_scores.device)
+        
+        thresh_tensor = thresh_tensor_lookup[label_preds]
+        
+        # thresh_tensor = box_scores.new_tensor([numeric_thresh_list[int(label)] for label in label_preds])
+        scores_mask = box_scores >= thresh_tensor
+        # 根据 mask 过滤
+        box_scores = box_scores[scores_mask]
+        box_preds = box_preds[scores_mask]
+        label_preds = label_preds[scores_mask]
+        
+    selected = []
+    if box_scores.shape[0] > 0:
+        box_scores_nms, indices = torch.topk(box_scores, k=min(nms_config.NMS_PRE_MAXSIZE, box_scores.shape[0]))
+        boxes_for_nms = box_preds[indices]
+        keep_idx, selected_scores = getattr(iou3d_nms_utils, nms_config.NMS_TYPE)(
+                boxes_for_nms[:, 0:7], box_scores_nms, nms_config.NMS_THRESH, **nms_config
+        )
+        selected = indices[keep_idx[:nms_config.NMS_POST_MAXSIZE]]
+
+    if multi_classes_score_thresh is not None:
+        original_idxs = scores_mask.nonzero().view(-1)
+        selected = original_idxs[selected]
+    return selected, src_box_scores[selected]
 
 def class_agnostic_nms(box_scores, box_preds, nms_config, score_thresh=None):
     src_box_scores = box_scores
